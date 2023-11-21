eb400a42abdc3e6cc85a187a57a06f3712d4a0cf
heewon <gmldnjs0317@gmail.com>
2023-11-21 21:25:49 +0900

    기능3  초안

--- sources/login_information.c
+++ sources/login_information.c
@@ -2,9 +2,15 @@
 #define SITE_NUM 10
 #define CHAR_NUM 100
 
+// TODO: 포인터를 이용한 함수 완성하기
+
 /* 로그인 정보의 주소를 포인터 배열에 저장하는 함수
 (행: 사이트, 1열: ID, 2열: password)*/
 void CopyAddress(int* arr1, int* arr2, int** arr_ptr);
+/*로그인 정보 삭제하는 함수*/
+void DelLogin(char* site[][CHAR_NUM], char* ID[][CHAR_NUM],
+              char* PW[][CHAR_NUM], int index);
+
 
 /*로그인 정보 관리 프로그램*/
 int main(void) {
@@ -33,7 +39,7 @@ int main(void) {
     if (choice == 1) {
       /*기능 1. 로그인 정보 저장*/
       /*기능 1.1 사용자로부터 로그인 정보 입력 받기*/
-      printf("사이트명: ");
+      printf("사이트: ");
       scanf_s("%s", site_name[site_count],
               (int)sizeof(site_name[site_count]));  // TODO: 동적 메모리 할당
       printf("ID: ");
@@ -41,7 +47,7 @@ int main(void) {
       printf("PW: ");
       scanf_s("%s", PW[site_count], (int)sizeof(PW[site_count]));
 
-      printf("사이트명: %s ", site_name[site_count]);
+      printf("사이트: %s ", site_name[site_count]);
       printf("ID: %s ", ID[site_count]);
       printf("PW: %s\n", PW[site_count]);
 
@@ -103,7 +109,82 @@ int main(void) {
       }
 
     } else if (choice == 3) {
-      /*기능 3. 로그인 정보 변경*/
+      while (1) 
+{
+        /*기능 3. 로그인 정보 수정*/
+        int choice_3;
+        printf("------------------------\n");
+        printf("원하시는 기능을 선택하세요.\n");
+        printf(
+            "1. 로그인 정보 삭제\n2. 로그인 정보 수정\n3. 뒤로 "
+            "가기\n");
+        printf("------------------------\n");
+        printf("번호: ");
+        scanf_s("%d", &choice_3);
+
+
+
+        
+
+        /*기능 3.1 로그인 정보 삭제*/
+        if (choice_3 == 1) {
+          int del_index;  // 삭제할 사이트 번호 저장 변수
+          
+          /*TODO: 전체 사이트 목록을 출력하는 코드 함수화*/ 
+          
+          // 전체 사이트 목록 출력
+          printf("-- 사이트 목록 (총 %d) --\n", site_count);
+          for (int i = 0; i < site_count; i++) {
+          printf("   %d. %s\n", i + 1, site_name[i]);
+          }
+
+          printf("삭제를 원하시는 사이트의 번호를 입력하세요: ");
+          scanf_s("%d", &del_index);
+
+          // TODO: 로그인 정보 삭제 하는 코드 작성
+          
+          // DelLogin(site_name, ID, PW, del_choice);
+          printf(
+              "%s의 로그인 정보가 정상적으로 삭제 되었습니다.", 
+              site_name[del_index - 1]);
+
+        }
+
+        /*기능 3.2 비밀번호 변경*/
+        else if (choice_3 == 2) {
+          int change_index;
+
+          // 전체 사이트 목록 출력
+          printf("-- 사이트 목록 (총 %d) --\n", site_count);
+          for (int i = 0; i < site_count; i++) {
+          printf("   %d. %s\n", i + 1, site_name[i]);
+          }
+
+          printf("비밀번호를 변경할 사이트를 선택해주세요: ");
+          scanf_s("%d", &change_index);
+
+          // 선택한 사이트의 현재 로그인 정보 출력
+          printf("----현재 로그인 정보----\n");
+          printf("사이트: %s\n아이디: %s\n비밀번호: %s\n",
+                 site_name[change_index - 1], ID[change_index - 1],
+                 PW[change_index - 1]);
+          printf("------------------------\n");
+
+          printf("변경할 비밀번호를 입력하세요: ");
+          scanf_s("%s", PW[change_index - 1],
+                  (int)sizeof(PW[change_index - 1]));
+
+          // 변경된 로그인 정보 출력
+          printf("---변경된 로그인 정보---\n");
+          printf("사이트: %s\n아이디: %s\n비밀번호: %s\n",
+                 site_name[change_index - 1], ID[change_index - 1],
+                 PW[change_index - 1]);
+          printf("------------------------\n");
+        } else {
+          break;
+        }
+      }
+
     } else if (choice == 4) {
       /*기능 4. 추천 비밀번호 생성*/
     } else if (choice == 5) {
@@ -116,4 +197,7 @@ int main(void) {
   }
 }
 
+// TODO: 함수 제대로 작동하도록 완성시키기
 void CopyAddress(char* arr, char** arr_ptr, int size) { arr_ptr[size] = arr; }
+void DelLogin(char* site[][CHAR_NUM], char* ID[][CHAR_NUM],
+              char* PW[][CHAR_NUM], int index) {}
